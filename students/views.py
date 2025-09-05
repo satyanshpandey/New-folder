a1569aa6205170e5ad17190e3f568adbae5830b6
satyanshpandey <satyansh234@gmail.com>
2025-09-05 12:38:18 +0530

    project setup and student app created

--- students/views.py
+++ students/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def students(request):
+    return HttpResponse("this is my firdt resposne.")
+
