bdc22278f2ae00b04780cb3da68a8ea3e43cae65
satyanshpandey <satyansh234@gmail.com>
2025-09-05 13:28:31 +0530

    best changes

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def students(request):
+    student = [
+        {
+        'naame':'satya',
+        'class': 'first class'
+    }
+    ]
+    return render(request , 'students.html' , {'student':student})
